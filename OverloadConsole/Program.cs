0e89d1a710b5c305280f11386feebdec475a2a90
ThansiJamal <thansijamal18@gmail.com>
2020-10-09 08:56:52 -0700

    add number

---
+++ OverloadConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadConsole
+{
+    class Program
+    {
+        public void add(int FirstNumber,int SecondNumber)
+        {
+            Console.WriteLine("Sum=" + (FirstNumber + SecondNumber));
+        }
+        public void add(int FirstNumber, int SecondNumber,int ThirdNumber)
+        {
+            Console.WriteLine("Sum=" + (FirstNumber + SecondNumber+ ThirdNumber));
+           
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.add(10, 20);
+            obj.add(10, 20,30);
+            Console.ReadKey();
+        }
+    }
+}
