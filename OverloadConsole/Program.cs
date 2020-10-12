4a22020f1ec861de6bce4c4a11cba32151219020
ThansiJamal <thansijamal18@gmail.com>
2020-10-12 10:06:02 -0700

    edit for commit

--- OverloadConsole/Program.cs
+++ OverloadConsole/Program.cs
@@ -8,20 +8,19 @@ namespace OverloadConsole
 {
     class Program
     {
-        public void add(int FirstNumber,int SecondNumber)
+        public static void Add(int FirstNumber, int SecondNumber)
         {
             Console.WriteLine("Sum=" + (FirstNumber + SecondNumber));
         }
-        public void add(int FirstNumber, int SecondNumber,int ThirdNumber)
+        public static void Add(int FirstNumber, int SecondNumber,int ThirdNumber)
         {
             Console.WriteLine("Sum=" + (FirstNumber + SecondNumber+ ThirdNumber));
            
         }
         static void Main(string[] args)
         {
-            Program obj = new Program();
-            obj.add(10, 20);
-            obj.add(10, 20,30);
+            Add(10 ,10);
+            Add(10, 20,30);
             Console.ReadKey();
         }
     }
